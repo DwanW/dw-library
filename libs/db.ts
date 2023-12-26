648b66e9112facd3582e79bb3df78b341f47273f
DwanW <kunvang@yahoo.com>
2023-12-26 15:45:43 -0700

    env changes

--- libs/db.ts
+++ libs/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_VOTE_URI || "");
+    await mongoose.connect(process.env.DB_URI || "");
     console.log("DB CONNECTED");
   } catch (e) {
     console.log(e);
